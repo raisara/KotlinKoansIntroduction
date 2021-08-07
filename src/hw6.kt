cb961af67344a49784464efa2edf53a071fbbeda
raisara <49322828+raisara@users.noreply.github.com>
2021-08-07 15:18:02 +0400

    Add files via upload

---
+++ src/hw6.kt
@@ -0,0 +1,17 @@
+fun sendMessageToClient(
+    client: Client?, message: String?, mailer: Mailer
+) {
+    if (client == null || message == null) return
+
+    val personalInfo: PersonalInfo = client.personalInfo ?: return
+
+    val email = personalInfo.email ?: return
+
+    mailer.sendMessage(email, message)
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
